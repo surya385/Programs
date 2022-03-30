1f847cce06acbd4d0b638912b39d91c07998c594
surya385 <surya26364@gmail.com>
2022-03-30 23:16:56 +0530

    Star pattern - Right triangle

---
+++ patternPrograms/RightTriangleStarPattern.java
@@ -0,0 +1,17 @@
+package programs.patternPrograms;
+
+public class RightTriangleStarPattern {
+
+	public static void main(String[] args) {
+		
+		for(int a=1;a<=10;a++)
+		{
+			for(int b=1;b<=a;b++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
