b0217d0cc5c88b07c41833d93c8e2c557998a353
surya385 <surya26364@gmail.com>
2022-04-01 21:06:42 +0530

    Pattern Program

---
+++ patternPrograms/F_InStarPattern.java
@@ -0,0 +1,25 @@
+package programs.patternPrograms;
+
+public class F_InStarPattern {
+
+	public static void main(String[] args) {
+		
+		for(int row=1;row<=9;row++)
+		{
+			if(row==1||row==5)
+			{
+				for(int star=1;star<=5;star++)
+				{
+				System.out.print("* ");
+				}
+			}
+			else
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
