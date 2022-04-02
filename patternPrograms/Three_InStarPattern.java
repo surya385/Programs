97f12060963fecfd3c2902077f8044e402c84694
surya385 <surya26364@gmail.com>
2022-04-02 16:31:01 +0530

    3 - Star Pattern

---
+++ patternPrograms/Three_InStarPattern.java
@@ -0,0 +1,34 @@
+package programs.patternPrograms;
+
+public class Three_InStarPattern {
+
+	public static void main(String[] args) {
+		
+		for(int row=1;row<=9;row++)
+		{
+			if(row==1||row==5||row==9)
+			{
+				for(int col=1;col<=4;col++)
+				{
+					System.out.print("* ");
+				}
+			}
+			else
+			{
+				for(int col=1;col<=5;col++)
+				{
+					if(col==5)
+					{
+						System.out.print("* ");
+					}
+					else
+					{
+						System.out.print("  ");
+					}
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
