15aa2e143a9803910041b026a01ca191426d9d5f
surya385 <surya26364@gmail.com>
2022-04-09 16:22:20 +0530

    Sort array elements in descending order

---
+++ SortArrayElementsInDescendingOrder.java
@@ -0,0 +1,44 @@
+package programs;
+
+import java.util.Scanner;
+
+public class SortArrayElementsInDescendingOrder {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("How many values do you want to enter :");
+		int count=s.nextInt();
+		int numbers[]=new int[count];
+		System.out.println("Enter the "+count+" values :");
+		for(int i=0;i<count;i++)
+		{
+			numbers[i]=s.nextInt();
+		}
+		System.out.println("Before sorting :");
+		for(int i=0;i<numbers.length;i++)
+		{
+			System.out.print(numbers[i]+" ");
+		}
+		System.out.println();
+		int temp=0;
+		for(int i=0;i<numbers.length;i++)
+		{
+			for(int j=0;j<numbers.length;j++)
+			{
+				if(numbers[i]>numbers[j])
+				{
+					temp=numbers[j];
+					numbers[j]=numbers[i];
+					numbers[i]=temp;
+				}
+			}
+		}
+		System.out.println("After sorting in descending order :");
+		for(int i=0;i<numbers.length;i++)
+		{
+			System.out.print(numbers[i]+" ");
+		}
+	}
+
+}
