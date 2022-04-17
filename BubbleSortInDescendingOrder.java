e84589637bd6edf6a37145fc6370e56d82725507
surya385 <surya26364@gmail.com>
2022-04-17 19:30:38 +0530

    Bubble Sorting - Descending Order

---
+++ BubbleSortInDescendingOrder.java
@@ -0,0 +1,42 @@
+package programs;
+
+import java.util.Scanner;
+
+public class BubbleSortInDescendingOrder {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter number of elements :");
+		int count=s.nextInt();
+		int numbers[]=new int[count];
+		System.out.println("Enter the "+count+" values :");
+		for(int i=0;i<count;i++)
+		{
+			numbers[i]=s.nextInt();
+		}
+		System.out.println("Before sorting :");
+		for(int i=0;i<numbers.length;i++)
+		{
+			System.out.print(numbers[i]+" ");
+		}
+		System.out.println();
+		int temp=0;
+		for(int i=1;i<numbers.length;i++)
+			for(int j=0;j<numbers.length-i;j++)
+			{
+				if(numbers[j]<numbers[j+1])
+				{
+					temp=numbers[j];
+					numbers[j]=numbers[j+1];
+					numbers[j+1]=temp;
+				}
+			}
+		System.out.println("Sorted array in descending order :");
+		for(int i:numbers)
+		{
+			System.out.print(i+" ");
+		}
+	}
+
+}
