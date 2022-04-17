73c230e4ee50965abb34d4c13bebf7835197af77
surya385 <surya26364@gmail.com>
2022-04-17 19:28:38 +0530

    Multiple Matrix Addition

---
+++ MatrixAdditionForMultipleMatrix.java
@@ -0,0 +1,55 @@
+package programs;
+
+import java.util.Scanner;
+
+public class MatrixAdditionForMultipleMatrix {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		
+		System.out.println("How many arrays do you want to add :"); 
+		int count=s.nextInt();
+		 
+		System.out.println("Enter how many row's for matrix :");
+		int row=s.nextInt();
+		System.out.println("Enter how many column's for matrix :");
+		int col=s.nextInt();
+		int a[][][]=new int[count][row][col];
+		for(int k=0;k<a.length;k++)
+		{
+			System.out.println("Enter the values for array -"+k);
+			for(int i=0;i<a[k].length;i++)
+			{
+				for(int j=0;j<a[k][i].length;j++)
+				{
+					
+					a[k][i][j]=s.nextInt();
+					
+				}
+			}
+		}
+		int b[][][]=new int[1][row][col];
+		for(int k=0;k<a.length;k++)
+		{
+		for(int i=0;i<a[0].length;i++)
+		{
+			for(int j=0;j<a[0][0].length;j++)
+			{
+			b[0][i][j]+=a[k][i][j];	
+			}
+		}
+		}
+		
+		System.out.println("Addition of two arrays :");
+		for(int i=0;i<b[0].length;i++)
+		{
+			for(int j=0;j<b[0][0].length;j++)
+			{
+				System.out.print(b[0][i][j]+"	");
+			}
+			System.out.println();
+		}
+	}
+
+}
