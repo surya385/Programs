2d68b3fe494507c50a9286e825422a2d4a3b6b1b
surya385 <surya26364@gmail.com>
2022-04-22 18:58:19 +0530

    Find Maximum And Minimum Value Of Array

--- MaxAndMinValueOfArray.java
+++ MaxAndMinValueOfArray.java
@@ -10,19 +10,18 @@ public class MaxAndMinValueOfArray {
 		Scanner s=new Scanner(System.in);
 		System.out.println("How many values do you want to compare");
 		int count=s.nextInt();
-		int number[]=new int[count];
+		long number[]=new long[count];
 		for(int i=0;i<count;i++)
 		{
-		System.out.println("Enter the number");
-		number[i]=s.nextInt();
+		number[i]=s.nextLong();
 		}
 		obj.max(number);
 		obj.min(number);
 		
 	}
 
-	private void max(int[] number) {
-		int max=0;
+	private void max(long[] number) {
+		long max=Long.MIN_VALUE;
 		for(int i=0;i<number.length;i++)
 		{
 			if(number[i]>max)
@@ -34,9 +33,9 @@ public class MaxAndMinValueOfArray {
 				
 	}
 
-	private void min(int[] number) {
-		int min=number[0];
-		for(int i=1;i<number.length;i++)
+	private void min(long[] number) {
+		long min=Long.MAX_VALUE;
+		for(int i=0;i<number.length;i++)
 		{
 			if(number[i]<min)
 			{
